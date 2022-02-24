fa355346aaf3d7dc7bd803ab631bbf4a82133cfd
kevin <kevinfalconi94@gmail.com>
2022-02-24 15:32:55 -0500

    onecambio

--- consulta/admin.py
+++ consulta/admin.py
@@ -6,6 +6,6 @@ class SintomasPacienteAdmin(admin.ModelAdmin):
     list_display = ('sintomas', 'descripcion','discapacidad','tipoDescapacidad')
 class CitaAdmin(admin.ModelAdmin):
     list_display = ('paciente', 'medico','turno','turno','fecha','estado')
-admin.site.register(Paciente)
+admin.site.register(Paciente, PacienteAdmin)
 admin.site.register(SintomasPaciente)
 admin.site.register(Cita)
