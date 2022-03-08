d82cf87abe022befe131460aad9707923ed0b173
Roger Pineda <roger.pinedapaz@gmail.com>
2022-03-08 12:08:01 -0500

    completando cambios basicos

--- entidad/models.py
+++ entidad/models.py
@@ -5,10 +5,16 @@ class Hospital(models.Model):
     sucursal = models.CharField(max_length=200)
     adress = models.CharField(max_length=200)
 
+    def __str__(self):
+        return self.sucursal
+
 class Especialidad(models.Model):
     hostpital = models.ForeignKey(Hospital, on_delete=models.CASCADE)
     especialidad = models.CharField(max_length=30)
     estado = models.CharField(max_length=30)
+    
+    def __str__(self):
+        return self.especialidad
 
 class Medico(models.Model):
     name = models.CharField(max_length=200)
